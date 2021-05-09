83fa9712c3bc52d5659051748b2ab257cf58d8ee
tdrk18 <s18.todoroki@gmail.com>
2021-05-09 17:03:30 +0900

    Parse '-size', '-s' options

--- main.go
+++ main.go
@@ -7,15 +7,22 @@ import (
 )
 
 func main() {
-	var outputName string
+	var (
+		outputName string
+		size       int
+	)
 	const (
 		defaultOutputName = "qr.png"
 		usageOutputName   = "Specify the output file name"
+		defaultSize       = 256
+		usageSize         = "Specify the frame size of output file"
 	)
 	flag.StringVar(&outputName, "output", defaultOutputName, usageOutputName)
 	flag.StringVar(&outputName, "o", defaultOutputName, usageOutputName+" (shorthand)")
+	flag.IntVar(&size, "size", defaultSize, usageSize)
+	flag.IntVar(&size, "s", defaultSize, usageSize+" (shorthand)")
 	flag.Parse()
-	err := qrcode.WriteFile("https://google.com", qrcode.Medium, 256, outputName)
+	err := qrcode.WriteFile("https://google.com", qrcode.Medium, size, outputName)
 	if err != nil {
 		log.Fatal(err)
 	}
