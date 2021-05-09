3433bfd87e68f6f50c4a9b9dcd42ccb276986400
tdrk18 <s18.todoroki@gmail.com>
2021-05-09 16:58:26 +0900

    Parse '-output', '-o' options

--- main.go
+++ main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"flag"
 	"github.com/skip2/go-qrcode"
 	"log"
 )
 
 func main() {
-	err := qrcode.WriteFile("https://google.com", qrcode.Medium, 256, "qr.png")
+	var outputName string
+	const (
+		defaultOutputName = "qr.png"
+		usageOutputName   = "Specify the output file name"
+	)
+	flag.StringVar(&outputName, "output", defaultOutputName, usageOutputName)
+	flag.StringVar(&outputName, "o", defaultOutputName, usageOutputName+" (shorthand)")
+	flag.Parse()
+	err := qrcode.WriteFile("https://google.com", qrcode.Medium, 256, outputName)
 	if err != nil {
 		log.Fatal(err)
 	}
