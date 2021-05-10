1b49d5dff06bf0dab228fbe0be7b63732d49977e
tdrk18 <s18.todoroki@gmail.com>
2021-05-10 13:30:41 +0900

    Parse '-content', '-c' options

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 
 func main() {
 	var (
+		content    string
 		outputName string
 		size       int
 	)
@@ -16,13 +17,18 @@ func main() {
 		usageOutputName   = "Specify the output file name"
 		defaultSize       = 256
 		usageSize         = "Specify the frame size of output file"
+		defaultContent    = ""
+		usageContent      = "Specify the content for QRCode"
 	)
 	flag.StringVar(&outputName, "output", defaultOutputName, usageOutputName)
 	flag.StringVar(&outputName, "o", defaultOutputName, usageOutputName+" (shorthand)")
 	flag.IntVar(&size, "size", defaultSize, usageSize)
 	flag.IntVar(&size, "s", defaultSize, usageSize+" (shorthand)")
+	flag.StringVar(&content, "content", defaultContent, usageContent)
+	flag.StringVar(&content, "c", defaultContent, usageContent+" (shorthand)")
 	flag.Parse()
-	err := qrcode.WriteFile("https://google.com", qrcode.Medium, size, outputName)
+
+	err := qrcode.WriteFile(content, qrcode.Medium, size, outputName)
 	if err != nil {
 		log.Fatal(err)
 	}
